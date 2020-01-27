c0839cec65c12aae6c343c4e04a191ccdf305fe4
류건우 <ryuken73@gmail.com>
2020-01-28 00:15:05 +0900

    delete unnecessarry js

--- insertDiv.js
+++
@@ -1,5 +0,0 @@
-console.log(document)
-const fixedDiv = document.createElement('div');
-fixedDiv.setAttribute('id', 'searchResult');
-fixedDiv.setAttribute("style", "position:fixed;top: 0;right: 0;width:200px;height:50%;z-index:10;background-color:yellow;");
-document.body.appendChild(fixedDiv)
