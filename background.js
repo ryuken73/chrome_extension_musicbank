9ce2c366a0b62fdf674f02cd28a1f8baca6328fc
ryuken73 <ryuken73@gmail.com>
2020-01-30 01:21:18 +0900

    trying react popup.html

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.webNavigation.onCompleted.addListener(function(details) {
     const {tabId,url} = details
 }, {url: [{urlMatches : 'http://musicbank.sbs.co.kr/'}]});
+
+// when browser connects wise.sbs.co.kr, popup.html activate
+chrome.runtime.onInstalled.addListener(function() {
+    // show popup.html 
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'musicbank.sbs.co.kr'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
